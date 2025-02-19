39337b726eb7f795f121b3f886a325444f6bb1cc
Atom <90292071+atfisch@users.noreply.github.com>
2025-02-19 10:54:19 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -1,5 +1,6 @@
 atomCount = 0;
 document.getElementById('atombtn').onclick() {
+  alert("hi",atomCount);
   atomCount += 1;
   document.getElementById("atoms").innerHTML = "Atoms: "+=atomCount.ToString();
 }
