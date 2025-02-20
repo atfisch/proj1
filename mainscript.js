d5c58dcbacbd28d5fabf436cc2e14e508966c70e
Atom <90292071+atfisch@users.noreply.github.com>
2025-02-20 12:37:50 -0500

    Update mainscript.js

--- mainscript.js
+++ mainscript.js
@@ -2,7 +2,7 @@ const atoms = document.getElementById("atoms");
 var atomCount = 0;
 
 function make_atom() {
-  alert(atomCount);
+  alert(atomCount)
   atomCount += 1;
   atoms.innerHTML = "Atoms: " + atomCount;
 }
